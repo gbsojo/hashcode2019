b9c0488e252d887fe68b0c1da855e2e499585cc5
Ma. Gabriela Rodriguez <gbsojo@Mas-MacBook-Pro.local>
2019-02-28 21:53:29 +0100

    VerticalSlides function

--- index.js
+++ index.js
@@ -60,15 +60,57 @@ function countSlides(photos) {
     }
 }
 
+function findVerticalSlides (verticalPhotos) {
+    const possibles = [];
+    verticalPhotos.forEach(function(photo, index) {
+        var best = {id2: photo.id};
+        const rest = verticalPhotos.slice(index+1);
+        rest.forEach(function(photo2) {
+            var common = getCommonTags(photo.tags, photo2.tags);
+            if (common.length > (best.common ? best.common : 0)) {
+                best.id = photo2.id;
+                best.common = common.length;
+            }
+        });
+        if (best.id) possibles.push(best);
+    });
+
+    return extractRepeat(possibles, []);
+}
+
+function extractBest (possibilities) {
+    if (possibilities.length > 1 ){
+        var best = {};
+        possibilities.forEach(function(slide) {
+            if (slide.common > (best.common ? best.common : 0)) {
+                best = slide;
+                console.log(best);
+            }
+        })
+        return best;
+    } else {
+        return possibilities[0];
+    }
+}
+
+function extractRepeat (possibilities, slides) {
+    if (possibilities.length < 2) {
+        slides.push(possibilities[0]);
+    }
+    else {
+        var best = extractBest(possibilities);
+        slides.push(best);
+        slides = extractRepeat(possibilities.filter(function(slide) {
+            return !(best && (slide.id == best.id || slide.id == best.id2 || slide.id2 == best.id || slide.id2 == best.id2))
+        }), slides);
+    }
+    return slides;
+}
+
 function interestFactor (s1, s2) {
     s1Tags = getSlideTags(s1);
     s2Tags = getSlideTags(s2);
-    const common = [];
-    s1Tags.forEach(function(s1tag) {
-        if (s2Tags.indexOf(s1tag) !== -1) {
-            common.push(s1tag);
-        }
-    })
+    const common = getCommonTags(s1Tags, s2Tags);
     return Math.min(s1Tags.length, s2Tags.length, common.length);
 };
 
@@ -79,3 +121,38 @@ function getSlideTags (s1) {
         return prev.concat(curr);
     });
 }
+
+function getCommonTags (s1Tags, s2Tags) {
+    var common = [];
+    s1Tags.forEach(function(s1tag) {
+        if (s2Tags.indexOf(s1tag) !== -1) {
+            common.push(s1tag);
+        }
+    })
+    return common;
+}
+
+const v = [
+    {
+        id: 0,
+        h: false,
+        tags: ['pepe', 'hola', 'cosa']
+    },
+    {
+        id: 1,
+        h: false,
+        tags: ['pepe', 'hola', 'cosa']
+    },
+    {
+        id: 2,
+        h: false,
+        tags: ['pepe3', 'hola', 'cosa']
+    },
+    {
+        id: 3,
+        h: false,
+        tags: ['pepe3', 'hola2', 'cosa']
+    },
+];
+
+console.log(findVerticalSlides(v));
